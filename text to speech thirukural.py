1ea89b3e8b1b59bef85a90935dac700f474e1d61
Sahidh-Khan <121621884+Sahidh-Khan@users.noreply.github.com>
2023-02-02 10:26:22 +0530

    Update text to speech thirukural.py

--- text to speech thirukural.py
+++ text to speech thirukural.py
@@ -9,4 +9,4 @@ language='ta' # mentioning the language
 obj=gTTS(text= text,lang=language,slow=False)  # mentioning the parameters
 
 obj.save('thirukural.mp3')  #to save the mp3 file
-os.system('thirukural.mp3')  #os.system() is used to execute a command.
+os.system('thirukural.mp3')  #os.system() is used to execute system command.
